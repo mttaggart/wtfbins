bf70e7e26abea6732e203b57dd1f1c75788b0a6b
Michael Taggart <michael.t.taggart@gmail.com>
2022-03-02 22:24:17 -0800

    Fix About layout

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -21,8 +21,8 @@
 @media (min-width: 1024px) {
   .about {
     min-height: 100vh;
-    display: flex;
-    align-items: center;
+    /* display: flex; */
+    /* align-items: center; */
   }
 }
 </style>
