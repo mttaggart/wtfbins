67152243d0447682adf5b775df8992300d51b4fd
Taggart <mtaggart@taggart-tech.com>
2022-03-02 17:49:34 -0800

    Start Vue

---
+++ planning.md
@@ -0,0 +1,23 @@
+# Project Planning
+
+## Data Scheme
+
+* `wtfbins`: `[WTFBin]`
+    * `id`: `Int`
+    * `name:` `String`
+    * `tags`: `[Tag]`
+    * `description`: `String`
+    * `contributor`: `String`
+    * `example:` `String`
+
+* `Tag`
+    `id`: `Int`
+    `name`: `String`
+
+## Pages
+
+* `/`
+    * WTFBin Detail (`/{id}`)
+* `/About`
+* `/Contribute`
+ 
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>WTFBins</h1>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+</style>
