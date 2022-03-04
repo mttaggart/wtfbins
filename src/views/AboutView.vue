5c0005b27b45223020a8488f571fef4556b863c9
Taggart <mtaggart@taggart-tech.com>
2022-03-04 14:06:09 -0800

    Remove size rule on About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -20,13 +20,3 @@
     </p>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    /* display: flex; */
-    /* align-items: center; */
-  }
-}
-</style>
