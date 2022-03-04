093aadca5f71d03474eacc695b0154d9b3901175
Michael Taggart <michael.t.taggart@gmail.com>
2022-03-04 09:08:23 -0800

    Start on modal

--- src/components/WtfBinItem.vue
+++ src/components/WtfBinItem.vue
@@ -3,11 +3,20 @@ import type {WtfBin} from "@/interfaces/WtfBin"
 defineProps<{
     wtfbin: WtfBin
 }>()
+
+let modalToggled = false;
+const toggleModal = () => {
+    console.log("Toggling Modal");
+    console.log(modalToggled);
+    modalToggled = !modalToggled;
+}
+
 </script>
 
 <template>
-    <li>
-        <h2>{{wtfbin.name}}</h2>
+    <li @click="toggleModal">
+        <h2>{{wtfbin.name}}<span v-if="modalToggled">*</span></h2>
+        
         <p>Contributed By: {{wtfbin.contributor}}</p>
         <p>{{wtfbin.description}}</p>
         <footer>
@@ -23,13 +32,20 @@ defineProps<{
         padding: 1rem;
         margin: 10px 0px;
         border-radius: 10px;
-        transition: all .3s;
+        font-weight: 400;
+        transition: box-shadow .3s;
         box-shadow: 1px 1px 1px black;
     }
 
     li:hover {
-        color: magenta;
+        font-weight: 800;
         box-shadow: 7px 7px 7px black;
         cursor: pointer;
     }
+
+    li:hover h2{
+        color: magenta;
+    }
+
+
 </style>
