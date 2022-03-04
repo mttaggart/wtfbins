21b3e7cb836d87cd4d92cc77b07a962595dc2424
Michael Taggart <michael.t.taggart@gmail.com>
2022-03-04 08:48:09 -0800

    Proper getter/action usage

--- src/stores/wtfbins.ts
+++ src/stores/wtfbins.ts
@@ -1,3 +1,4 @@
+import type { WtfBin } from '@/interfaces/WtfBin';
 import { defineStore } from 'pinia'
 
 const binsUrl = "https://raw.githubusercontent.com/mttaggart/wtfbins/main/wtfbins.json";
@@ -6,8 +7,26 @@ export const wtfBinStore = defineStore({
   id: 'wtfbin',
   state: () => ({
     wtfbins: [],
+    textFilter: "",
+    tagFilter: "",
   }),
   getters: {
+    filterByText(state) {
+      const textFilter = state.textFilter;
+      console.log(textFilter);
+      if (textFilter == "") return state.wtfbins;
+      return state.wtfbins.filter((b: WtfBin) => {
+        const lowerName = b.name.toLowerCase();
+        const lowerDesc = b.description.toLowerCase();
+        const lowerContrib = b.contributor.toLowerCase();
+        return (
+          lowerName.indexOf(textFilter) >= 0 ||
+          lowerDesc.toLowerCase().indexOf(textFilter) >= 0 ||
+          lowerContrib.toLowerCase().indexOf(textFilter) >= 0
+        );
+      } 
+      );
+    }
   },
   actions: {
     retrieveWtfBins: async function() {
@@ -15,6 +34,10 @@ export const wtfBinStore = defineStore({
       let binsData = await binsJson.json();
       console.log(binsData);
       this.wtfbins = binsData;
+    },
+
+    updateTextFilter(textFilter: string) {
+      this.textFilter = textFilter;
     }
   }
 })
