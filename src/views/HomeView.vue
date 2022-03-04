c8232a76c5494f3c82e99090f66af30638038f45
Michael Taggart <michael.t.taggart@gmail.com>
2022-03-04 08:48:30 -0800

    Working text search

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
   import WtfBinItem from "@/components/WtfBinItem.vue"
+  import WtfBinTextSearchVue from "@/components/WtfBinTextSearch.vue";
   import { wtfBinStore } from "@/stores/wtfbins";
   const store = wtfBinStore();
   store.retrieveWtfBins();
-  
+  const updateTextFilter = (e: Event) => {
+    let newFilter = (e.target as HTMLInputElement).value.toLowerCase();
+    store.updateTextFilter(newFilter);
+  }  
 </script>
-
 
 <template>
   <main>
+    <WtfBinTextSearchVue :update-handler="updateTextFilter" />
     <p>These are WTFBins. I don't know what anyone was thinking.</p>
     <ul>
-      <WtfBinItem v-for="(w, i) in store.wtfbins" :key="i" :wtfbin="w" wtf/>  
+      <WtfBinItem v-for="(w, i) in store.filterByText" :key="i" :wtfbin="w" wtf/>  
     </ul>
   </main>
 </template>
