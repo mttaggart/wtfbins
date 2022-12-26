5c7513368012b8ff8c9d0d81a36b4e77254c675d
Michael Taggart <mtaggart@taggart-tech.com>
2022-12-26 12:57:44 -0800

    Remove unused import

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,6 @@
 import Navbar from "./navbar";
 import Footer from "./footer";
 import Head from "next/head"
-import { ReactElement } from "react";
 
 export default function Layout({ children }) {
     return (
